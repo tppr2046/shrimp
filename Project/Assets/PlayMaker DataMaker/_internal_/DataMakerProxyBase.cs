9a4a5b52ebb0634bcc344991ebd6a32a17aef6c4
hayatos <mac@mac.lan>
2014-08-03 09:54:29 +0800

    get a wrong

--- Project/Assets/PlayMaker DataMaker/_internal_/DataMakerProxyBase.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class DataMakerProxyBase : MonoBehaviour {
-	
-	// the reference Name used when finding or when several component coexists on the same GameObject
-	public string referenceName = "";
-}
